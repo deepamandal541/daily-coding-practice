f7854adedbfe78b8cd424e4d4e0182aee525c36d
deepamandal541 <deepamandal541@gmail.com>
2026-06-07 12:38:20 +0530

    Create hello world.java

---
+++ hello world.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String args[]) {
+       System.out.println("Hello, World.");
+       System.out.println("Hello, Java.");
+    }
+}
+
+
+Question
+  In this challenge, you will learn how to print “Hello World!” in standard output (stdout).
+
+Complete the code by copying the two lines of code below and pasting them inside the body of your main method.
+
+For Java:
+
+System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
